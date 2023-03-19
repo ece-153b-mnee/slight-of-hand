c9e288aea8550d05744efa57cb69cba8743b3bb9
Matthew Nguyen <matthewnguyen@ucsb.edu>
2023-03-18 17:24:55 -0700

    python scripts now handles brightness

--- bluetooth.py
+++ bluetooth.py
@@ -1,6 +1,16 @@
 import serial
+import time
+# import serial.tools.list_ports
+
 
 serialPort = serial.Serial(port='/dev/cu.DSDTECHHC-05', baudrate=9600, timeout=1, stopbits=1)
+
+
+# ports = serial.tools.list_ports.comports()
+
+# for port, desc, hwid in sorted(ports):
+#         print("{}: {} [{}]".format(port, desc, hwid))
+
 
 while 1:
     data = serialPort.readline().decode('utf-8')
@@ -8,6 +18,7 @@ while 1:
         measurement = int(data)
         print(measurement)
     except:
-        print("")
+        pass
     # print(data)
+    # time.sleep(1)
     
--- lightcontrol.py
+++ lightcontrol.py
@@ -5,12 +5,6 @@ import serial
 
 def main():
     num_lights = None
-    if len(sys.argv) != 2:
-        print("\nDiscovery will go much faster if you provide the number of lights on your LAN:")
-        print("  python {} <number of lights on LAN>\n".format(sys.argv[0]))
-    else:
-        num_lights = int(sys.argv[1])
-
     # instantiate LifxLAN client, num_lights may be None (unknown).
     # In fact, you don't need to provide LifxLAN with the number of bulbs at all.
     # lifx = LifxLAN() works just as well. Knowing the number of bulbs in advance 
@@ -22,21 +16,41 @@ def main():
     devices = lifx.get_lights()
     bulb = devices[0]
 
+    # bluetooth
+    bt = serial.Serial(port='/dev/cu.DSDTECHHC-05', baudrate=9600, timeout=1, stopbits=1)
+
     while True:
         # polling to check for bluetooth signal
-        command = input("type 'b' to change brightness, 'k' to change temperature: ")
-        if (command == 'b'):
-            brightness = input("enter brightness [0-65535]: ")
-            if (brightness < 0) or (brightness > 65535):
-                print("outside of range, try again")
-            bulb.set_brightness(brightness, 500)
-        elif (command == 'k'):
-            kelvin = input("enter temperature [2500-9000]: ")
-            if (kelvin < 2500) or (kelvin > 9000):
-                print("outside of range, try again")
-            bulb.set_colortemp(kelvin, 500)
-        else:
-            print("invalid command, try again")
+        data = bt.readline().decode('utf-8')
+        try:
+            measurement = int(data)
+            if measurement in range(0,3):
+                # low -> 1-2 inches
+                bulb.set_brightness(1000, 500)
+            elif measurement in range(3, 6):
+                # low-medium -> 3-5 inches
+                bulb.set_brightness(10000, 500)
+            elif measurement in range(6, 9):
+                # high-medium -> 6-8 inches
+                bulb.set_brightness(30000, 500)
+            elif measurement in range(9, 11):
+                # high -> 9-10 inches
+                bulb.set_brightness(50000, 500)
+        except:
+            pass
+        # command = input("type 'b' to change brightness, 'k' to change temperature: ")
+        # if (command == 'b'):
+        #     brightness = input("enter brightness [0-65535]: ")
+        #     if (int(brightness) < 0) or (int(brightness) > 65535):
+        #         print("outside of range, try again")
+        #     bulb.set_brightness(brightness, 500)
+        # elif (command == 'k'):
+        #     kelvin = input("enter temperature [2500-9000]: ")
+        #     if (int(kelvin) < 2500) or (int(kelvin) > 9000):
+        #         print("outside of range, try again")
+        #     bulb.set_colortemp(kelvin, 500)
+        # else:
+        #     print("invalid command, try again")
 
         # brightness in range [0-65535], duration in ms, rapid = 1 ->
         # bulb.set_brightness() # (brightness, [duration], [rapid])
